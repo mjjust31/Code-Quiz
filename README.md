57807531a33aa881034587ff2e48cb3f7660dd37
justmann <mj.justmann@gmail.com>
2023-08-12 11:56:10 -0500

    able-to-remove-choice-buttons

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ AS A coding boot camp student
 I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
 SO THAT I can gauge my progress compared to my peers
 
-
-
 GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question
@@ -20,42 +18,37 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score
 
-
-1. I click the start button to begin the quiz. 
+1. I click the start button to begin the quiz.
    a. the instructions are erased.
    b. the timer begins.
    c. View high scores in top left hand stay.
+
 - completed
 
-
-
 - fuctions for displaying questions (loop).
--function for displaying choices. (loop).
-- need to verify the correct answer. 
+  -function for displaying choices. (loop).
+- need to verify the correct answer.
 - If else statement for correct answer (add 10 points), else {deduct the time and deduct score}
+
 2. The first question appears. There are four options.
-   a. when I click the correct answer, the question is removed and a new question appears. 
-        i. the screen shows the user that the answer was correct.
+   a. when I click the correct answer, the question is removed and a new question appears.
+   i. the screen shows the user that the answer was correct.
 
-  b. when I click the wrong answer, the question is removed and a new question apepars. 
-        i. the scresn show the user that the answer was wrong. 
-        ii. time is deducted from the timer.
+b. when I click the wrong answer, the question is removed and a new question apepars.
+i. the scresn show the user that the answer was wrong.
+ii. time is deducted from the timer.
 
-
-
-
-
-
-3. When the questions are complete. the quiz ends and the timer stops. 
+3. When the questions are complete. the quiz ends and the timer stops.
 4. a score is calculated based on the correct answers.
-5. the user can enter their initials with a submit button. 
-6. Once I click submit, the user's score and intials are saved to the high score. 
-7. I am taken to the high score page. 
+5. the user can enter their initials with a submit button.
+6. Once I click submit, the user's score and intials are saved to the high score.
+7. I am taken to the high score page.
 8. I see two buttons:
-    i. When I click "go back", the quiz starts over. 
-    ii. When I click "clear high scores", it clears all saved high scores.
+   i. When I click "go back", the quiz starts over.
+   ii. When I click "clear high scores", it clears all saved high scores.
 
+   Using Object methods to use as quiz info. I reviewed the following material to help me understand how to set up the object of quizInfo. 1.https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/ 2. https://www.youtube.com/watch?v=gnbzqtd2zgM
 
-    Using Object methods to use as quiz info. I reviewed the following material to help me understand how to set up the object of quizInfo.
-    1.https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
-    2. https://www.youtube.com/watch?v=gnbzqtd2zgM
+9. I wanted to see if I can could generate the questions randomly. I realized I needed to pass a function into the function displayQuestionWithChoices. Looked at this code to determine how to remove option from an array. https://stackoverflow.com/questions/37963358/how-can-i-make-a-random-array-with-no-repeats and incorporated it into my code.
+
+10. Had difficulty reoving the previous choice buttons. Came up with a solution to create a fuction and reuse it after click event for either right or wrong answer. 
--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -2,15 +2,15 @@
 
 var navBar = document.querySelector(".first-page-top");
 var highScoreLink = document.querySelector(".high-score-link"); //needs to be a anchor tag
-var timer = document.querySelector(".timer");
+var timerEl = document.querySelector(".timer");
 var instructions = document.querySelector(".game-instructions");
 var title = document.querySelector(".quiz-title");
 var startButton = document.querySelector(".button-start");
 var titlePage = document.querySelector(".game-start");
 var quizContainer = document.querySelector(".quiz-container");
+var scoreEl = document.querySelector(".score");
 
-var questionIndex = 0;
-var choiceIndex = 0;
+var quizInfoIndex = 0;
 
 var quizInfo = [
   {
@@ -21,31 +21,31 @@ var quizInfo = [
       "Hype Text Make Langauage",
       "Hyper Texting Makeup Language",
     ],
-    answer: 0,
+    answer: "Hypertext Markup Language",
   },
 
   {
     question: "Which one is known as a Primitive Data Type?",
     choices: ["Booleans", "Strings", "Number", "All of the Above"],
-    answer: 3,
+    answer: "All of the Above",
   },
 
   {
     question: "Inside which HTML element do we put the JavaScript?",
     choices: ["<js>", "<script>", "<javascript", "<scripting"],
-    answer: 1,
+    answer: "<script>",
   },
   {
     question:
       "Which CSS property is used to change the text color of an element?",
     choices: ["fgcolor", "text-color", "color", "font-color"],
-    answer: 2,
+    answer: "color",
   },
 
   {
     question: "Choose the correct HTML element for the largest heading:",
-    choices: ["<h6>", "<heading>", "<head>", "<h1>"],
-    answer: 3,
+    choices: ["<h6>", "<heading>", "<head>", "<h1>", "<p>"],
+    answer: "<h1>",
   },
 ];
 
@@ -53,22 +53,25 @@ var quizInfo = [
 //you can add a single event listener to the parent element and use event delegation to handle the click event for all the choices.
 
 //start of game and main-page
-var timeLeft = 60;
+var timeLeft = 70;
 var score = 0;
-timer.textContent = timeLeft;
+timerEl.textContent = timeLeft;
 title.textContent = "Code Quiz Challenge";
 highScoreLink.textContent = "View High Scores";
+scoreEl.textContent = "Score: " + score;
+
 instructions.textContent =
   "Try to answer the following code-related questions withing the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
 
 //start of quiz
+
 startButton.addEventListener("click", function () {
   titlePage.setAttribute("style", "display:none;");
-  timer.textContent = timeLeft;
+  timerEl.textContent = timeLeft;
 
   var timeInterval = setInterval(function () {
     timeLeft--;
-    timer.textContent = timeLeft;
+    timerEl.textContent = timeLeft;
 
     if (timeLeft === 0) {
       clearInterval(timeInterval);
@@ -76,7 +79,6 @@ startButton.addEventListener("click", function () {
   }, 1000);
   displayQuestionWithChoices(0);
 });
-
 
 //this will display the question index when it is called with choices
 function displayQuestionWithChoices(quizInfoIndex) {
@@ -91,6 +93,50 @@ function displayQuestionWithChoices(quizInfoIndex) {
   }
 }
 
+quizContainer.addEventListener("click", function (event) {
+  var choiceButton = event.target;
 
-//create a function for all choices
-// var questionEl = document.getElementById(".question");
+  if (choiceButton.matches("button")) {
+    // console.log(choiceButton.textContent)
+    if (choiceButton.textContent === quizInfo[quizInfoIndex].answer) {
+      console.log("Correct Answer!");
+      score = score + 10;
+      scoreEl.textContent = "Score: " + score;
+      removeButtons();
+    } else {
+      console.log("incorrect answer");
+      timeLeft = timeLeft - 15;
+      score = score - 5;
+      scoreEl.textContent = "Score: " + score;
+      removeButtons();
+    }
+
+    quizInfoIndex++;
+    displayQuestionWithChoices(quizInfoIndex); //cycles through questions, but keeps all buttons created
+  }
+});
+
+if (timeLeft === 0) {
+  console.log("end game");
+}
+
+//resuable code in two placed for removing previous buttons.
+function removeButtons() {
+  var removeButtons = document.querySelectorAll("button");
+  removeButtons.forEach(function (removeButtons) {
+    removeButtons.remove();
+  });
+}
+
+//can I cratea random index number to pull the question randomly?
+//will need an if statement of what to do when array is gone.
+// function randomQuestionGenerator() {
+//   var indexNumber = [0, 1, 2, 3, 4];
+//   var random = Math.floor(Math.random() * indexNumber.length);
+//   var num = indexNumber[random];
+//   indexNumber.splice(random, 1);
+//   return num;
+// }
+// var randomQuestionSet = randomQuestionGenerator();
+
+//next we will need to verify the user's click with the corrct answer.
