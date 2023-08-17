fb2348ada7eb6062d6cb24924da2e2b2798dc7e4
justmann <mj.justmann@gmail.com>
2023-08-17 11:35:39 -0500

    removed-buttons-under-answerContainer

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -48,6 +48,7 @@ var enterIntials = document.querySelector(".enterIntials");
 var displayOutcome = document.querySelector(".displayOutcome");
 var lastPage = document.querySelector(".last-page");
 var submitFormEl = document.getElementById("submitForm");
+var answerContainerEl = document.querySelector(".answerContainer");
 
 var timeLeft = 70;
 var score = 0;
@@ -59,7 +60,7 @@ function init() {
   highScoreLink.textContent = "View High Scores";
   scoreEl.textContent = "Score: " + score;
   instructions.textContent =
-    "Try to answer the following code-related questions withing the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
+    "Try to answer the following code-related questions withing the time limit. Finish the quiz as quickly as possible for more points. Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
 
   startButton.addEventListener("click", startQuiz);
 }
@@ -76,6 +77,7 @@ function startQuiz() {
       clearInterval(timeInterval);
       highScoreForm();
     } else if (quizInfoIndex > quizInfo.length - 1) {
+      scoreByTime();
       clearInterval(timeInterval);
       highScoreForm();
     }
@@ -83,18 +85,34 @@ function startQuiz() {
   displayQuestionWithChoices(quizInfoIndex);
 }
 
+function scoreByTime() {
+  if (timeLeft > 50) {
+    score += 30;
+    scoreEl.textContent = "Score: " + score;
+  } else if (timeLeft > 30) {
+    score += 15;
+    scoreEl.textContent = "Score: " + score;
+  } else if (timeLeft > 15) {
+    score += 5;
+    scoreEl.textContent = "Score: " + score;
+  }
+}
 
+scoreEl.textContent = "Score: " + score;
+// var quizContainer= document.querySelector(".answerContainer");
 //this will display the question index when it is called with choices
 function displayQuestionWithChoices(quizInfoIndex) {
+  quizContainer.setAttribute("style", "display:block");
+
   for (var i = 0; i < quizInfo[quizInfoIndex].choices.length; i++) {
     var questionEl = document.querySelector(".question");
     questionEl.textContent = quizInfo[quizInfoIndex].question;
+
     var choiceButton = document.createElement("button");
     choiceButton.textContent = quizInfo[quizInfoIndex].choices[i];
-    quizContainer.appendChild(choiceButton);
+    answerContainerEl.appendChild(choiceButton);
   }
   quizContainer.addEventListener("click", displayAnswer);
-  displayAnswer();
 }
 
 function displayAnswer(event) {
@@ -102,11 +120,13 @@ function displayAnswer(event) {
   if (selected.matches("button")) {
     if (selected.textContent === quizInfo[quizInfoIndex].answer) {
       correctAnswer();
+      removeButtons();
     } else {
       wrongAnswer();
+      removeButtons();
     }
   }
-
+  removeButtons();
   quizInfoIndex++;
   displayQuestionWithChoices(quizInfoIndex);
 }
@@ -131,7 +151,6 @@ function correctAnswer() {
   displayOutcome.setAttribute("style", "color:green;");
   score += 10;
   scoreEl.textContent = "Score: " + score;
-  removeButtons();
 }
 
 function wrongAnswer() {
@@ -140,34 +159,73 @@ function wrongAnswer() {
   displayOutcome.setAttribute("style", "color:red; font-style:italic;");
   score -= 3;
   scoreEl.textContent = "Score: " + score;
-  removeButtons();
 }
 
 function removeButtons() {
-  var removeButtons = document.querySelectorAll("button");
-  removeButtons.forEach(function (button) {
+  var answerContainerEl = document.querySelector(".answerContainer");
+  var buttons = answerContainerEl.querySelectorAll("button");
+
+  buttons.forEach(function (button) {
     button.remove();
   });
 }
 
-function handleFormSubmit(event) {
+// function removeButtons() {
+//   while (answerContainerEl.firstchild) {
+//     answerContainerEl.removeChild(answerContainerEl.firstChild);
+//   }
+// }
+var saveHighScoreEl = document.querySelector(".saveHighScore");
+
+saveHighScoreEl.addEventListener("click", function (event) {
   event.preventDefault();
 
-  var scoreList = document.createElement("li");
-  var initials = document.getElementById("initials").value;
+  var initialsEl = document.getElementById("initials").value.trim();
 
-  var highScoreEl = document.getElementById("highScoreList");
-  highScoreEl.appendChild(scoreList);
+  var finalScore = {
+    score: score,
+    initials: initialsEl.value,
+  };
+  localStorage.setItem("finalScore", JSON.stringify(finalScore));
+  initialsEl.value = "";
+  window.location.href = "./highScore.html";
+});
 
-  document.getElementById("initials").value = "";
-
-  localStorage.setItem("score", score);
-  localStorage.setItem("initials", initials);
+function renderHighScore() {
+  var lastFinalScore = JSON.parse(localStorage.getItem("finalScore"));
+  if (finalScore !== null) {
+    var listItem = document.createElement("li");
+    listItem.textContent = quizInfo[quizInfoIndex].choices[i];
+  }
 }
 
-submitFormEl.addEventListener("submit", handleFormSubmit);
+// function getFromLocal() {
+//   localStorage.setItem("score", score);
+//   localStorage.setItem("initials", initials);
+// }
+
+// function highScoreForm() {
+
+// }
 
 init();
+
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+
+//   var scoreList = document.createElement("li");
+//   var initials = document.getElementById("initials").value;
+
+//   var highScoreEl = document.getElementById("highScoreList");
+//   highScoreEl.appendChild(scoreList);
+
+//   document.getElementById("initials").value = "";
+
+//   localStorage.setItem("score", score);
+//   localStorage.setItem("initials", initials);
+// }
+
+// submitFormEl.addEventListener("submit", handleFormSubmit);
 
 // function highScorePage() {
 //   if (timeLeft === 0 || quizInfoIndex >= quizInfo.length-1) {
